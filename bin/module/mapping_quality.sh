cca35f2ac618d8b0bbd9062a14caa8fedb971c9e
Nicholas D. Socci <soccin@mskcc.org>
2019-03-19 15:29:07 -0400

    Reverted to orig version to test with samtools 1.19

--- bin/module/mapping_quality.sh
+++ bin/module/mapping_quality.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 numb_of_files=$1
-out=$2
-experiment=$3
-outcontrol=$4
+out=$2 
+experiment=$3 
+outcontrol=$4 
 quality=$5
-cutsite=$6
-
+cutsite=$6 
 echo 'Filtering the quality of the mapped reads ...'
 if [[ -z "$cutsite" && $numb_of_files == 1 ]]; then # IF THERE IS NO ENZYME && WITH SE READS
-    echo "Probably broken for newer version of samtools see case 2"
-    exit
     samtools view -Sb -q $quality $out/$experiment.sam > $out/$experiment.q$quality.bam
     samtools sort $out/$experiment.q$quality.bam -o $out/$experiment.q$quality.sorted.bam
     samtools index $out/$experiment.q$quality.sorted.bam
 fi
 if [[ -z "$cutsite" && $numb_of_files == 2 ]]; then # IF THERE IS NO ENZYME && WITH PE READS
     samtools view -Sb -f 66 -q $quality $out/$experiment.sam > $out/$experiment.q$quality.bam
-    samtools sort $out/$experiment.q$quality.bam $out/$experiment.q$quality.sorted
+    samtools sort $out/$experiment.q$quality.bam -o $out/$experiment.q$quality.sorted.bam
     samtools index $out/$experiment.q$quality.sorted.bam
 fi
 if [[ ! -z "$cutsite" && $numb_of_files == 2 ]]; then # IF THERE IS ENZYME && WITH PE READS
-    echo "Probably broken for newer version of samtools see case 2"
-    exit
     samtools view -Sb -f 66 -q $quality $out/$experiment.sam > $out/$experiment.q$quality.bam & pid1=$!
     samtools view -Sb -f 66 -q $quality $outcontrol/$experiment.sam > $outcontrol/$experiment.q$quality.bam & pid2=$!
     wait $pid1
@@ -36,8 +31,6 @@ if [[ ! -z "$cutsite" && $numb_of_files == 2 ]]; then # IF THERE IS ENZYME && WI
     wait $pid2
 fi
 if [[ ! -z "$cutsite" && $numb_of_files == 1 ]]; then # IF THERE IS ENZYME && WITH SE READS
-    echo "Probably broken for newer version of samtools see case 2"
-    exit
     samtools view -Sb -q $quality $out/$experiment.sam > $out/$experiment.q$quality.bam & pid1=$!
     samtools view -Sb -q $quality $outcontrol/$experiment.sam > $outcontrol/$experiment.q$quality.bam & pid2=$!
     wait $pid1
